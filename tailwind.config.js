d2ef12e938523d767e0457afb8642a631712908c
yusuf alvian <yusufalvian16@gmail.com>
2023-05-17 19:58:54 +0700

    mengeluarkan index keluar supaya bisa dipakai di github pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.{html,js}"],
   theme: {
     extend: {
       container: {
